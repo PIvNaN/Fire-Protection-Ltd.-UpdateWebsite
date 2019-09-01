950d1e230815fc01bb2291f5275a5a08c7459236
Iva Petkova <iva.g.petkova@abv.bg>
2019-09-01 21:06:56 +0300

    feat: start update of Fire Protection website

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import './styles/global/reset'
+</style>
